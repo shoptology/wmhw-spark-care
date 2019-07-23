99fcd98b1336d2b68da179dda1e549138789e11d
Clifford Nelson <cliffordryannelson@gmail.com>
2019-07-22 20:19:33 -0700

    sent spark confirmation modal

---
+++ src/app/_components/share-spark-modal/share-spark-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShareSparkModalComponent } from './share-spark-modal.component';
+
+describe('ShareSparkModalComponent', () => {
+  let component: ShareSparkModalComponent;
+  let fixture: ComponentFixture<ShareSparkModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShareSparkModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShareSparkModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/_components/share-spark-modal/share-spark-modal.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-share-spark-modal',
+  templateUrl: './share-spark-modal.component.html',
+  styleUrls: ['./share-spark-modal.component.scss']
+})
+export class ShareSparkModalComponent implements OnInit {
+
+  public toAssociate: string;
+  public username: string;
+  public type: string;
+  public message: string;
+  nextTimer;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public dialogRef: MatDialogRef<ShareSparkModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+  ) { }
+
+  ngOnInit() {
+
+    this.username = localStorage.getItem('username');
+
+    // the modal shall only be open for a brief moment
+    this.nextTimer = setTimeout(()=>{
+      this._navNext();
+    }, 4000);
+
+  }
+
+  onNoClick(): void {
+    alert('close');
+    this.dialogRef.close();
+  }
+
+  closeDialog() {
+    alert('close');
+    this.dialogRef.close('Pizza!');
+  }
+
+  public done(): void {
+    this._navNext();
+  }
+
+  private _navNext(): void {
+    this.dialogRef.close();
+    this.router.navigate(["/home"]);
+  }
+
+  ngOnDestroy(){
+    clearTimeout(this.nextTimer);
+  }
+
+}
