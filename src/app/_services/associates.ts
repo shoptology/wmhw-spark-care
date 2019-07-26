ed5d566b8de7912013ae3b732b7054784bffa336
Clifford Nelson <cliffordryannelson@gmail.com>
2019-07-25 17:02:29 -0700

    add a few shoptology team members to our store

--- src/app/_services/associates.ts
+++ src/app/_services/associates.ts
@@ -6,7 +6,87 @@ export const ASSOCIATES: Associate[] = [
 
   // test accounts
   {
-    win: 135792468,
+    win: 132547698,
+    storeId: 100000,
+    name: 'Mark Roy',
+    email: 'Mark.Roy@walmart.com',
+    isManager: false,
+    managersWin: 135792472,
+  },
+  {
+    win: 132547821,
+    storeId: 100000,
+    name: 'Meredith Garmon',
+    email: 'Meredith.Garmon@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132547944,
+    storeId: 100000,
+    name: 'David Stutts',
+    email: 'David.Stutts@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548067,
+    storeId: 100000,
+    name: 'Joshua Narofsky',
+    email: 'Joshua.Narofsky@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548190,
+    storeId: 100000,
+    name: 'LPH',
+    email: 'LPH@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548313,
+    storeId: 100000,
+    name: 'Rieke',
+    email: 'Rieke@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548436,
+    storeId: 100000,
+    name: 'Ryan Karlstrom',
+    email: 'Ryan.Karlstrom@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548559,
+    storeId: 100000,
+    name: 'Stacy Schrack',
+    email: 'Stacy.Schrack@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548682,
+    storeId: 100000,
+    name: 'Whitney Fleschler',
+    email: 'Whitney.Fleschler@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548805,
+    storeId: 100000,
+    name: '',
+    email: '@walmart.com',
+    isManager: true,
+    managersWin: 135792472,
+  },
+  {
+    win: 132548928,
     storeId: 100000,
     name: 'Charlie Anderson',
     email: 'charlie.anderson@walmart.com',
@@ -14,7 +94,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792470,
+    win: 132549051,
     storeId: 100000,
     name: 'Scott Schlee',
     email: 'scott.schlee@walmart.com',
@@ -22,7 +102,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792468,
   },
   {
-    win: 135792504,
+    win: 132549174,
     storeId: 100000,
     name: 'Clifford Nelson',
     email: 'clifford.nelson@walmart.com',
@@ -32,7 +112,7 @@ export const ASSOCIATES: Associate[] = [
 
   // sudo real accounts
   {
-    win: 135792472,
+    win: 132549297,
     storeId: 123456,
     name: 'Matt Parry',
     email: 'Matt.Parry@walmart.com',
@@ -40,7 +120,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792474,
+    win: 132549420,
     storeId: 123456,
     name: 'Nicole Daigle',
     email: 'Nicole.Daigle@walmart.com',
@@ -48,15 +128,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792476,
-    storeId: 123456,
-    name: 'Mark Roy',
-    email: 'Mark.Roy@walmart.com',
-    isManager: false,
-    managersWin: 135792472,
-  },
-  {
-    win: 135792478,
+    win: 132549543,
     storeId: 123456,
     name: 'Aleena Stubbs',
     email: 'Aleena.Stubbs@walmart.com',
@@ -64,7 +136,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792480,
+    win: 132549666,
     storeId: 123456,
     name: 'Toby Martinez',
     email: 'Toby.Martinez@walmart.com',
@@ -72,7 +144,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792482,
+    win: 132549789,
     storeId: 123456,
     name: 'Sumayya Crosby',
     email: 'Sumayya.Crosby@walmart.com',
@@ -80,7 +152,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792484,
+    win: 132549912,
     storeId: 123456,
     name: 'Danial Liu',
     email: 'Danial.Liu@walmart.com',
@@ -88,7 +160,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792486,
+    win: 132550035,
     storeId: 123456,
     name: 'Bradley Farmer',
     email: 'Bradley.Farmer@walmart.com',
@@ -96,7 +168,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792488,
+    win: 132550158,
     storeId: 123456,
     name: 'Eryk Short',
     email: 'Eryk.Short@walmart.com',
@@ -104,7 +176,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792490,
+    win: 132550281,
     storeId: 123456,
     name: 'Elora Nolan',
     email: 'Elora.Nolan@walmart.com',
@@ -112,7 +184,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792492,
+    win: 132550404,
     storeId: 123456,
     name: 'Aaliyah Alvarado',
     email: 'Aaliyah.Alvarado@walmart.com',
@@ -120,7 +192,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792494,
+    win: 132550527,
     storeId: 123456,
     name: 'Carley Santiago',
     email: 'Carley.Santiago@walmart.com',
@@ -128,7 +200,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792496,
+    win: 132550650,
     storeId: 123456,
     name: 'Ava-Grace Fulton',
     email: 'Ava-Grace.Fulton@walmart.com',
@@ -136,7 +208,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792498,
+    win: 132550773,
     storeId: 123456,
     name: 'Martyna Fernandez',
     email: 'Martyna.Fernandez@walmart.com',
@@ -144,7 +216,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792500,
+    win: 132550896,
     storeId: 123456,
     name: 'Amar Dixon',
     email: 'Amar.Dixon@walmart.com',
@@ -152,7 +224,7 @@ export const ASSOCIATES: Associate[] = [
     managersWin: 135792472,
   },
   {
-    win: 135792502,
+    win: 132551019,
     storeId: 123456,
     name: 'Evelina Wainwright',
     email: 'Evelina.Wainwright@walmart.com',
