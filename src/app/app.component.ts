7a6a6289ac8a7e55e4ce53e2376b4cd383ccfb4d
Clifford Nelson <cliffordryannelson@gmail.com>
2019-07-29 11:48:29 -0700

    make toolbar fixed position at top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { routerTransition } from './app-router.animations';
 
 @Component({
@@ -8,6 +8,20 @@ import { routerTransition } from './app-router.animations';
   animations: [ routerTransition ],
 })
 export class AppComponent {
+
+  @ViewChild('toolbar') toolbar: ElementRef;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+
+    // add/remove shadow on toolbar (8 instead of 0 to add padding to the scroll)
+    if( window.pageYOffset > 8 ){
+      this.toolbar.nativeElement.classList.add('app__toolbar--scrolled');
+    } else {
+      this.toolbar.nativeElement.classList.remove('app__toolbar--scrolled')
+    }
+
+  }
 
   title = 'wmhw-spark-care';
 
